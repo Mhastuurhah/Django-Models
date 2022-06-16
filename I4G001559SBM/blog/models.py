4e5ae4a18a74a24102f3b135462fe38a13aa76a1
Masturah Moshood <abidexmosh@yahoo.com>
2022-06-16 20:45:58 +0100

    django models

---
+++ I4G001559SBM/blog/models.py
@@ -0,0 +1,11 @@
+from statistics import mode
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
